2062a838c0a2d4d201a21898b74759109e66e4da
Jeroen Hodenius <jeroen@fruitcake.nl>
2020-09-13 01:33:43 +0200

    Change command name

--- app/Console/Commands/CreateAdminUser.php
+++ app/Console/Commands/CreateAdminUser.php
@@ -14,7 +14,7 @@ class CreateAdminUser extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'admin:create-or-restore-admin';
 
     /**
      * The console command description.
